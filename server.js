7c13c57038348cad2349ebe1eea1f339fa688b6b
Eylül Uçar <eylullucarr@outlook.com.tr>
2023-05-22 14:52:34 +0300

    hotfix

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
-const port = 1600;
+const port = 1800;
 const userroutes=require('./src/routes')
+const cors = require('cors');
 
+app.use(cors());
 app.use(express.json());
 
 
